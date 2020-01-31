eeeacb6b48fc631eae6bc9541dad7a930657776b
Ronda D Cowen <cowenhome5@gmail.com>
2020-01-31 21:50:09 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM pledges
   JOIN projects
   ON projects.id = pledges.project_id
-  GROUP BY (SUM (pledges.amount) - (projects.funding_goal))=> 0
+  GROUP BY (SUM (pledges.amount) - (projects.funding_goal)) >= 0
   HAVING pledges.amount >= projects.funding_goal";
 
 end
