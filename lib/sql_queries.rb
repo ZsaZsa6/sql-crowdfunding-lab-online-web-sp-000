57be1536f1b41ce7fa08501cb088455f25cf2ecb
Ronda D Cowen <cowenhome5@gmail.com>
2020-01-31 21:40:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,8 +28,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM pledges
   JOIN projects
   ON projects.id = pledges.project_id
-  GROUP BY amount_left
-  HAVING pledges.amount >= projects.funding_goal";
+  GROUP BY pledges.amount >= projects.funding_goal";
 
 end
 
